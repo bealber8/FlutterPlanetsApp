f9cc8ca6e9aa52437ebf8ad55781bc370d461403
bealber8 <beacalle1997@gmail.com>
2019-09-08 17:49:25 +0100

    Design of AppBar

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:planetsapp/ui/home/home_page.dart';
+
+void main() {
+  //Routes.initRoutes();
+  runApp(new MaterialApp(
+    title: "Planets",
+    home: new HomePage(),  
+  ));
+}
+
+
